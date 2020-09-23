88c97f4b4928101a327167b404d6baebbd32b8f9
Ilay <iliusha_1999nooh@mail.ru>
2020-09-23 07:49:24 +0300

    m2-task-3

--- Module-2/task-3.js
+++ Module-2/task-3.js
@@ -1,10 +1,12 @@
 let s = 0
 let p = 0
 
-Num:for(p = +prompt('Введите число') ;p != null ;p = +prompt('Введите число')){
-    if(p != Number){
+
+Num:for(p = prompt('Введите число') ;p != null ;p = prompt('Введите число')){
+
+    if(+p !== +p ){
+        alert('Вы ввели не число, попробуйте еще раз.')
         continue Num;
     } else (s = +p + +s)
 }
 alert(s)
-
