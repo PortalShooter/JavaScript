df5d438cbb523f29c5b83714d7de454c5dbf1599
Ilay <iliusha_1999nooh@mail.ru>
2020-09-24 08:38:59 +0300

    fix

--- Module-2/task-2.js
+++ Module-2/task-2.js
@@ -1,8 +1,13 @@
-let firsYear = prompt('Введите год начала')
-let lastYear = prompt('Введите последний год')
+let firsYear = +prompt('Введите год начала');
+let lastYear = +prompt('Введите последний год');
 
-Number: for(firsYear ; firsYear <= lastYear ; firsYear++){
-    if(firsYear % 4 == 0){
-        console.log(firsYear)
-    } else continue Number;
+if(firsYear != firsYear || lastYear != lastYear){
+    alert('Вы ввели не число/числа, попробуйте еще раз');
+}
+
+for(firsYear ; firsYear <= lastYear ; firsYear++){
+    
+    if(firsYear % 4 == 0 && firsYear % 100 != 0 || firsYear % 400 == 0){
+        console.log(firsYear);
+    } else continue;
 }
--- Module-2/task-3.js
+++ Module-2/task-3.js
@@ -1,12 +1,13 @@
-let s = 0
-let p = 0
+let sum = 0
+let term = 0
 
 
-Num:for(p = prompt('Введите число') ;p != null ;p = prompt('Введите число')){
+Num:while(term != null){
+    term = prompt('Введите число')
 
-    if(+p !== +p ){
+    if(+term !== +term ){
         alert('Вы ввели не число, попробуйте еще раз.')
         continue Num;
-    } else (s = +p + +s)
+    } else (sum = +term + +sum)
 }
-alert(s)
+alert(sum)
