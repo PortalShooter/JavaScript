517372c168fb825149f2f46bc1b124c01256c2f5
Ilay <iliusha_1999nooh@mail.ru>
2020-09-24 08:46:06 +0300

    one more fix

--- Module-2/task-2.js
+++ Module-2/task-2.js
@@ -1,13 +1,13 @@
 let firsYear = +prompt('Введите год начала');
 let lastYear = +prompt('Введите последний год');
 
-if(firsYear != firsYear || lastYear != lastYear){
+if(firsYear !== firsYear || lastYear !== lastYear){
     alert('Вы ввели не число/числа, попробуйте еще раз');
-}
+};
 
 for(firsYear ; firsYear <= lastYear ; firsYear++){
     
-    if(firsYear % 4 == 0 && firsYear % 100 != 0 || firsYear % 400 == 0){
+    if(firsYear % 4 === 0 && firsYear % 100 !== 0 || firsYear % 400 === 0){
         console.log(firsYear);
     } else continue;
-}
+};
--- Module-2/task-3.js
+++ Module-2/task-3.js
@@ -1,13 +1,13 @@
-let sum = 0
-let term = 0
+let sum = 0;
+let term = 0;
 
 
-Num:while(term != null){
-    term = prompt('Введите число')
+Num:while(term !== null){
+    term = prompt('Введите число');
 
     if(+term !== +term ){
-        alert('Вы ввели не число, попробуйте еще раз.')
+        alert('Вы ввели не число, попробуйте еще раз.');
         continue Num;
-    } else (sum = +term + +sum)
-}
-alert(sum)
+    } else (sum = +term + +sum);
+};
+alert(sum);
