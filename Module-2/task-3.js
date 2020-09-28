1a4c51f6ebc5d925cc3dd6983875a05932e317aa
Ilay <iliusha_1999nooh@mail.ru>
2020-09-28 08:31:05 +0300

    fixes

--- Module-2/task-3.js
+++ Module-2/task-3.js
@@ -2,12 +2,11 @@ let sum = 0;
 let term = 0;
 
 
-Num:while(term !== null){
+while(term !== null){
     term = prompt('Введите число');
 
-    if(+term !== +term ){
+    if(isNaN(term)){
         alert('Вы ввели не число, попробуйте еще раз.');
-        continue Num;
     } else (sum = +term + +sum);
 };
 alert(sum);
