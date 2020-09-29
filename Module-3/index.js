9aef9f85b1178b957a670e9ebe03d1abc8e65ea9
Ilay <iliusha_1999nooh@mail.ru>
2020-09-29 08:46:27 +0300

    module3

---
+++ Module-3/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let randomNumber = Math.floor(Math.random() * (1000 - 1) + 1)
+
+function guessNumber(randomNumber){
+    let version = prompt('Угадай, какое число я загадал? \nПодсказка: от 1 до 1000')
+    while(version != null){
+        if(isNaN(version) === true){
+            alert('Введите число!');
+        } else if(+version === randomNumber){
+            alert('Правильно!');
+            break;
+        } else if(version < randomNumber){
+            alert('Больше');
+        } else if(version > randomNumber){
+            alert('Меньше');
+        }
+        version = prompt('Попробуй еще раз');
+    }
+}
+function newGame(){
+    confirm('Не хотите попробовать еще раз?') ? guessNumber(randomNumber) : alert('Тогда в следующий раз');
+}
+
+guessNumber(randomNumber)
+newGame()
